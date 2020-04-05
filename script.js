3f156d088fa32341bf81bd6285457fc2be5b3562
mrthom11 <32447751+mrthom11@users.noreply.github.com>
2020-04-05 12:22:30 +0200

    Add files via upload
    bazze

--- script.js
+++ script.js
@@ -2,7 +2,5 @@ function myFunction() {
   var x = document.getElementById("carriera");
   if (x.style.display === "none") {
     x.style.display = "block";
-  } else {
-    x.style.display = "none";
   }
 }
