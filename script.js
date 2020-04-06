8527ec69c3b1e200e5b04745603481dc23579946
vezzaniludovica <32447836+vezzaniludovica@users.noreply.github.com>
2020-04-06 19:49:03 +0200

    doppia scelta attivata

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function myFunction() {
-  var x = document.getElementById("carriera");
+function giuridici() {
+  var x = document.getElementById("giuridici");
   if (x.style.display === "none") {
     x.style.display = "block";
   }
+}
+
+function filosofici(){
+	var y = document.getElementById("filosofici");
+	if (y.style.display==="none"){
+		y.style.display ="block";
+	}
 }
