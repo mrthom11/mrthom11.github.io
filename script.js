8ddccf06b3a02333b626eae0aaa3a29894a0db9a
vezzaniludovica <32447836+vezzaniludovica@users.noreply.github.com>
2020-04-06 19:18:44 +0200

    sistemati errori

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 function myFunction() {
   var x = document.getElementById("carriera");
   if (x.style.display === "none") {
     x.style.display = "block";
   }
-=======
-function myFunction() {
-  var x = document.getElementById("carriera");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  }
->>>>>>> 3f156d088fa32341bf81bd6285457fc2be5b3562
 }
