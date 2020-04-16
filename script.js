d61baa7f029d4049a1cf9cb9b3dc740d83bf1161
vezzaniludovica <32447836+vezzaniludovica@users.noreply.github.com>
2020-04-16 15:35:06 +0200

    proseguito giuridici

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
+function godown(){
+  window.scrollTo(0,document.body.scrollHeight);
+}
 function giuridici() {
   var x = document.getElementById("giuridici");
   if (x.style.display === "none") {
     x.style.display = "block";
   }
 }
-
 function filosofici(){
 	var y = document.getElementById("filosofici");
 	if (y.style.display==="none"){
 		y.style.display ="block";
+	}
+}
+function avvocato(){
+	var z = document.getElementById("avvocato");
+	if (z.style.display==="none"){
+    z.style.display ="block";
+    window.scrollTo(0,document.body.scrollHeight);
+
+	}
+}
+function politica(){
+	var a = document.getElementById("politica");
+	if (a.style.display==="none"){
+    a.style.display ="block";
+    window.scrollTo(0,document.body.scrollHeight);
+
 	}
 }
