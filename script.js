cc2ffb3a5c6f1d6cc84d3716dc2cd480c6045bb5
vezzaniludovica <32447836+vezzaniludovica@users.noreply.github.com>
2020-04-19 15:58:54 +0200

    prova-disabilitare-bottoni

--- script.js
+++ script.js
@@ -1,41 +1,38 @@
-function intermezzo(){
-	var intermezzo = document.getElementById("intermezzo");
-	if (intermezzo.style.display == "none") {
-		intermezzo.style.display = "block";
-	}
-}
+const buttonf=document.getElementById("btn1");
+const buttong=document.getElementById("btn2");
 
-function godown(){
-  window.scrollTo(0,document.body.scrollHeight);
+function filosofici(){ 
+	var y = document.getElementById("filosofici");
+	if (y.style.display==="none"){
+		y.style.display ="block";
+		window.scrollTo(0,document.body.scrollHeight);
+	}
+	buttonf.disabled=true;
+	buttong.disabled=true;
 }
 function giuridici() {
-  var x = document.getElementById("giuridici");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  }
-}
-function filosofici(){
-	var y = document.getElementById("filosofici");
-	if (y.style.display == "none"){
-		y.style.display = "block";
-		intermezzo();
+	var x = document.getElementById("giuridici");
+	if (x.style.display === "none") {
+	  x.style.display = "block";
+	  window.scrollTo(0,document.body.scrollHeight);
 	}
+	buttong.disabled=true;
+	buttonf.disabled=true;
+  }
+	  
 
-}
 function avvocato(){
 	var z = document.getElementById("avvocato");
 	if (z.style.display==="none"){
     z.style.display ="block";
     window.scrollTo(0,document.body.scrollHeight);
-
 	}
+
 }
 function politica(){
 	var a = document.getElementById("politica");
 	if (a.style.display==="none"){
     a.style.display ="block";
     window.scrollTo(0,document.body.scrollHeight);
-
 	}
 }
-
